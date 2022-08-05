60ebb8245c875a4b554d78a6191c4f2b324a0268
Oliver Perez <oliver.celica@gmail.com>
2022-08-04 22:30:10 -0500

    keywords

--- src/Academy/Programming/Main.java
+++ src/Academy/Programming/Main.java
@@ -3,9 +3,38 @@ package Academy.Programming;
 public class Main {
 
     public static void main(String[] args){
-        //Section 4 Expressions and statements
+
+        double scoreC=10000;
+        double levelCompleted =8;
+        double bonus= 200;
+        boolean gameOver = true;
+
+        if (gameOver == true) {
+            double finalScore = scoreC + (levelCompleted*bonus);
+            System.out.println("YOUR FINAL SCORE WAS " + finalScore);
+
+        }
+
+
+
+
+    }
+    public static void calculateScore() {
+        double scoreC=10000;
+        double levelCompleted =8;
+        double bonus= 200;
+        boolean gameOver = true;
+
+        if (gameOver == true) {
+            double finalScore = scoreC + (levelCompleted*bonus);
+            System.out.println("YOUR FINAL SCORE WAS " + finalScore);
+
+        }
+
     }
         }
+
+
 
 
 
